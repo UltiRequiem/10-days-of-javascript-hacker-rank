55bb914b5e5b71cc2159a7b88cceb6031caab820
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-02 19:06:54 -0500

    :building_construction: Order Directorys

---
+++ src/08-for.js
@@ -0,0 +1,14 @@
+function vowelsAndConsonants(s) {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  for (let i = 0; i < s.length; i++) {
+    if (vowels.indexOf(s[i]) > -1) {
+      console.log(s[i]);
+    }
+  }
+
+  for (let j = 0; j < s.length; j++) {
+    if (vowels.indexOf(s[j]) < 0) {
+      console.log(s[j]);
+    }
+  }
+}
