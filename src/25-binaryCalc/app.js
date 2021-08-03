eaa7cf7f34c260c22c7248a128a90c03ce6164ff
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-08-02 20:49:29 -0500

    :sparkles: Finished

---
+++ src/25-binaryCalc/app.js
@@ -0,0 +1,22 @@
+document.getElementById('btn0').addEventListener('click', () => {
+  document.getElementById('res').insertAdjacentHTML('beforeend', '0');
+});
+
+document.getElementById('btn1').addEventListener('click', () => {
+  document.getElementById('res').insertAdjacentHTML('beforeend', '1');
+});
+
+function operatorAction(eve) {
+  document
+    .getElementById('res')
+    .insertAdjacentHTML('beforeend', eve.target.innerHTML);
+}
+
+document.getElementById('btnSum').onclick = operatorAction;
+document.getElementById('btnMul').onclick = operatorAction;
+document.getElementById('btnDiv').onclick = operatorAction;
+document.getElementById('btnSub').onclick = operatorAction;
+
+document.getElementById('btnClr').onclick = () => {
+  document.getElementById('res').innerHTML = '';
+};
